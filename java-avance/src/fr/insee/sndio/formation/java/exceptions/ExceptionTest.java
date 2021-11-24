18f5d3a59c9051f19bc47cc6c6974e3586c75a5b
Minh NGUYEN <minh.nguyen@insee.fr>
2021-11-24 11:11:16 +0100

    exceptions

--- java-avance/src/fr/insee/sndio/formation/java/exceptions/ExceptionTest.java
+++ java-avance/src/fr/insee/sndio/formation/java/exceptions/ExceptionTest.java
@@ -2,14 +2,40 @@ package fr.insee.sndio.formation.java.exceptions;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+
 import org.junit.jupiter.api.Test;
 
 class ExceptionTest {
 
+	private String message;
 	@Test
 	void test() {
 		GestionException gestionException = new GestionException();
-		gestionException.lireNombre(0);
+		gestionException.lireNombre(4);
+		
 	}
+	
+//	@Test
+	void npeTest() {
+		mettreEnMajuscule("coucou");
+		mettreEnMajuscule(message);
+		mettreEnMajuscule("coucou2");
+	}
+	
+//	@Test
+	void checkedTest() {
+		GestionException gestionException = new GestionException();
+		try {
+			gestionException.lireFichierTexte();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void mettreEnMajuscule(String message) {		
+		System.out.println(message.toUpperCase()); //on oublie de vérifier message != null
+	}	
 
 }
--- java-avance/src/fr/insee/sndio/formation/java/exceptions/GestionException.java
+++ java-avance/src/fr/insee/sndio/formation/java/exceptions/GestionException.java
@@ -1,5 +1,10 @@
 package fr.insee.sndio.formation.java.exceptions;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,10 +16,36 @@ public class GestionException {
 		listeNombre.add(1);
 		listeNombre.add(2);
 		listeNombre.add(3);
+		listeNombre.add(0);
 	}
 
 	public void lireNombre(int index) {
-		System.out.println("Le nombre en position " 
-			+ index + " est :  " + listeNombre.get(index));
+		try {			
+			System.out.println("Le nombre en position " + index + " est :  " + listeNombre.get(index));
+			System.out.println("Le résultat de l'index divisé par ce nombre :  " 
+					+ index/listeNombre.get(index));
+		}
+		catch (IndexOutOfBoundsException | ArithmeticException e) {
+			e.printStackTrace();
+		}
+//		catch (ArithmeticException e) {
+//			e.printStackTrace();
+//		}
+		// éventuellement :
+		finally {
+			System.out.println("Fin de l'exécution");
+		}	
 	}
+	
+	public void lireFichierTexte () throws IOException  { //pour l'exemple, pas à faire
+		Path path = Paths.get("C:\\Users\\kivt18\\Documents\\data\\data.txt");		
+		BufferedReader bufferedReader = Files.newBufferedReader(path);
+		String ligne = bufferedReader.readLine();
+		while(ligne != null) {
+			System.out.println(ligne);
+			ligne = bufferedReader.readLine();
+		}		
+		bufferedReader.close();
+	}
+	
 }
